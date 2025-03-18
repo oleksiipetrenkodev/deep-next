e85c2aef2b323707fec141779376c7b15f74e89e
oleksiipetrenkodev <apetrenko.job@gmail.com>
2025-03-18 20:36:29 +0200

    food app, added few pages, components, db with meals

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,7 @@
+export default function MealDetailsPage({ params }) {
+  return (
+    <main>
+      <h1>Meal {params.mealSlug}</h1>
+    </main>
+  );
+}
---
+++ components/main-header/main-header.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import logoImg from "@/assets/logo.png";
+import MainHeaderBackground from "./main-header-background";
+import classes from "./main-header.module.css";
+import Image from "next/image";
+import NavLink from "./nav-link";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logoImg} alt="NextLevel Food" priority />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
