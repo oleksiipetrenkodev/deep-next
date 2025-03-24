7bfc213c3838c89eb9c8fe84f4f8d2edca24e5d6
oleksiipetrenkodev <apetrenko.job@gmail.com>
2025-03-24 10:26:01 +0200

    disaply a one meal

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,7 +1,40 @@
-export default function MealDetailsPage({ params }) {
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
+
+export default async function MealDetailsPage({ params }) {
+  const { mealSlug } = params;
+
+  const meal = await getMeal(mealSlug);
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replaceAll("\n", "<br />");
+
   return (
-    <main>
-      <h1>Meal {params.mealSlug}</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image} alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>Meal {meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>John Doe</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
