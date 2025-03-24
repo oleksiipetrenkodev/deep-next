948baf2230102c96af7f956bed597e5ec530adf0
oleksiipetrenkodev <apetrenko.job@gmail.com>
2025-03-24 17:06:08 +0200

    added image picker and display image preview

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,3 +1,4 @@
+import ImagePicker from "../image-picker";
 import classes from "./page.module.css";
 
 export default function ShareMealPage() {
@@ -38,7 +39,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          IMAGE PICKER
+          <ImagePicker label="Add an image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
